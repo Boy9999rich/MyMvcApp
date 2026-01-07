29732dede5f489f5b82c1527760e90764ac030b7
Boymurod Mamarajabov <boymurodmamarajapov1@gmail.com>
2026-01-07 20:44:24 +0500

    mm

--- MyMvcApp/Controllers/ProductsController.cs
+++ MyMvcApp/Controllers/ProductsController.cs
@@ -14,21 +14,72 @@ namespace MyMvcApp.Controllers
             _context = context;
         }
 
+        // GET ALL
         public async Task<IActionResult> Index()
         {
             var products = await _context.Products.ToListAsync();
             return View(products);
         }
 
+        // GET BY ID (DETAILS)
+        public async Task<IActionResult> Details(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null) return NotFound();
+
+            return View(product);
+        }
+
+        // CREATE (GET)
         public IActionResult Create()
         {
             return View();
         }
 
+        // CREATE (POST)
         [HttpPost]
         public async Task<IActionResult> Create(Product product)
         {
             _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        // EDIT (GET)
+        public async Task<IActionResult> Edit(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null) return NotFound();
+
+            return View(product);
+        }
+
+        // EDIT (POST)
+        [HttpPost]
+        public async Task<IActionResult> Edit(Product product)
+        {
+            _context.Products.Update(product);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        // DELETE (GET)
+        public async Task<IActionResult> Delete(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null) return NotFound();
+
+            return View(product);
+        }
+
+        // DELETE (POST)
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null) return NotFound();
+
+            _context.Products.Remove(product);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
